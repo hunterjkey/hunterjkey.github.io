293b35ce81417d8171eceaa5baef1f8828465add
Hunter Key <hunterjkey@gmail.com>
2019-02-07 09:29:06 -0800

    Added Javascript

---
+++ Javascript.js
@@ -0,0 +1,8 @@
+function Highlight(obj) {
+  obj.style.border = "1px solid red";
+  obj.style.color = "red";
+}
+function DeHighlight(obj) {
+  obj.style.border = "1px solid black";
+  obj.style.color = "black";
+}
