1e57b89f231a5f874e4e5f4eccb81669a60ce6d2
Hunter Key <hunterjkey@gmail.com>
2019-02-07 19:50:39 -0800

    added color picker to portfolio

--- Javascript.js
+++ Javascript.js
@@ -6,3 +6,27 @@ function DeHighlight(obj) {
   obj.style.border = "1px solid black";
   obj.style.color = "black";
 }
+
+function ChangeGrey() {
+	document.body.style.backgroundColor = "#909090";
+}
+
+function ChangeRed() {
+	document.body.style.backgroundColor = "red";
+}
+
+function ChangeYellow() {
+	document.body.style.backgroundColor = "yellow";
+}
+
+function ChangeGreen() {
+	document.body.style.backgroundColor = "green";
+}
+
+function ChangeBlue() {
+	document.body.style.backgroundColor = "#4484CE";
+}
+
+function ChangePurple() {
+	document.body.style.backgroundColor = "purple";
+}
